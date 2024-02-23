92550c30b44b661b0bae76e8b9c68f38ccabd9bf
José Filho <100032235+josecsfilho@users.noreply.github.com>
2024-02-22 22:19:14 -0300

    Incluido templestes funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import home, FuncionariosList
 
 
 urlpatterns = [
-    path('', home),
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
 ]
