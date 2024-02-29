d99bc48c4c34eaa79b55333dbb5fa3f069c3c97f
José Filho <100032235+josecsfilho@users.noreply.github.com>
2024-02-28 22:19:15 -0300

    Continuação do CRUD incluindo etapa do update.

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import home, FuncionariosList
+from .views import FuncionariosList, FuncionarioEdit
 
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionarios'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from .models import Funcionario
 
 
@@ -11,3 +11,7 @@ class FuncionariosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         queryset = Funcionario.objects.filter(empresa=empresa_logada)
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
