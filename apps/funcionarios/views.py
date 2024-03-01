321a484c4e6fa5c1b91e7ebe146c6a7bed65069e
José Filho <100032235+josecsfilho@users.noreply.github.com>
2024-02-29 21:53:10 -0300

    Inserido class delete do CRUD

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import Funcionario
 
 
@@ -10,8 +11,13 @@ class FuncionariosList(ListView):
 
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
-        queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        return Funcionario.objects.filter(empresa=empresa_logada)
 
 class FuncionarioEdit(UpdateView):
     model = Funcionario
     fields = ['nome', 'departamentos']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
