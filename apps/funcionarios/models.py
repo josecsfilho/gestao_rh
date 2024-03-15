ce41aca8f0ae8d4a1de9da64d2dc47f316a8eb97
jose.filho <100032235+josecsfilho@users.noreply.github.com>
2024-03-14 22:46:37 -0300

    Inserido tela com total de horas extra dentro da funcionario form

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from django.urls import reverse
 
 from apps.departamentos.models import Departamento
@@ -11,10 +12,15 @@ class Funcionario(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, unique=True)
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+    imagem = models.ImageField()
 
 
     def get_absolute_url(self):
         return reverse('list_funcionario')
 
+    @property
+    def total_horas_extra(self):
+        total = self.registrohoraextra_set.all().aggregate(Sum('horas'))['horas__sum']
+        return total
     def __str__(self):
         return self.nome
