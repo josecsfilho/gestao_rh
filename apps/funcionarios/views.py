394838e9b43eb6a6fa01346ff15fc786b2da8fe0
jose.filho <100032235+josecsfilho@users.noreply.github.com>
2024-03-26 21:28:45 -0300

    Com esse código É possível exportar CSV e Excel

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from django.views import View
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView, TemplateView
 from .models import Funcionario
 
 from django.template.loader import get_template
@@ -42,6 +43,37 @@ class FuncionarioNovo(CreateView):
         return super(FuncionarioNovo, self).form_valid(form)
 
 
+def relatorio_funcionarios(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(200, 810, 'Relatorio de funcionarios')
+
+    funcionarios = Funcionario.objects.filter(
+        empresa=request.user.funcionario.empresa)
+
+    str_ = 'Nome: %s | Hora Extra: %.2f'
+
+    p.drawString(0, 800, '_' * 150)
+
+    y = 750
+    for funcionario in funcionarios:
+        p.drawString(10, y, str_ % (
+            funcionario.nome, funcionario.total_horas_extra))
+        y -= 20
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
 class Render:
     @staticmethod
     def render(path: str, params: dict, filename: str):
@@ -52,3 +84,17 @@ class Render:
 
 
 
+
+class Pdf(View):
+
+    def get(self, request):
+        params = {
+            'today': 'Variavel today',
+            'sales': 'Variavel sales',
+            'request': request,
+        }
+        return Render.render('funcionarios/relatorio.html', params, 'myfile')
+
+
+class PdfDebug(TemplateView):
+    template_name = 'funcionarios/relatorio.html'
