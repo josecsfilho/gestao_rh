3c50679c6743834e0da626440483daeb5bc0aefe
jose.filho <100032235+josecsfilho@users.noreply.github.com>
2024-04-02 22:21:43 -0300

    Melhoria da template do dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,7 +7,7 @@ from apps.registro_hora_extra.models import RegistroHoraExtra
 from apps.departamentos.models import Departamento
 from django.core import serializers
 from django.http import HttpResponse
-# from .tasks import send_relatorio
+from .tasks import send_relatorio
 from django.db.models import Sum
 
 
@@ -16,7 +16,7 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     funcionario = request.user.funcionario
-    # data['total_funcionarios'] = funcionario.empresa.total_funcionarios
+    data['total_funcionarios'] = funcionario.empresa.total_funcionarios
     data['total_funcionarios_ferias'] = funcionario.empresa.total_funcionarios_ferias
     data['total_funcionarios_doc_pendente'] = funcionario.empresa.total_funcionarios_doc_pendente
     data['total_funcionarios_doc_ok'] = funcionario.empresa.total_funcionarios_doc_ok
@@ -29,9 +29,9 @@ def home(request):
     return render(request, 'core/index.html', data)
 
 
-# def celery(request):
-#     send_relatorio.delay()
-#     return HttpResponse('Tarefa incluida na fila para execucao')
+def celery(request):
+    send_relatorio.delay()
+    return HttpResponse('Tarefa incluida na fila para execucao')
 
 
 def departamentos_ajax(request):
